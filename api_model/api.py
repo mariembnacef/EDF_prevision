43c67d3dd50926bcfeacb4f65683f350318d8e29
Mohando22 <belkacemimohand@outlook.com>
2025-05-18 15:23:35 +0000

    add

--- api_model/api.py
+++ api_model/api.py
@@ -148,4 +148,4 @@ def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8003, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8001, reload=True)
